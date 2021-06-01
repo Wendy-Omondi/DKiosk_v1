c5128728d3019c692d55ec944f3dc0917debc4c9
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-06-01 23:59:35 +0000

    renamed from SignUp to dashboard

---
+++ src/components/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import NavbarAll from '../NavbarAll';
+
+function Dashboard () {
+    return (
+        <>
+            <NavbarAll/>
+            <h1 className='dashboard'>DASHBOARD</h1>
+        </>
+    )
+}
+
+export default Dashboard;
