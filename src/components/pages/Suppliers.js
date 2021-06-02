ab635276ca41d3867c2dffc5f7aad690f708842b
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-06-02 00:17:45 +0000

    added admin only page

---
+++ src/components/pages/Suppliers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import NavbarAll from '../NavbarAll';
+
+function Suppliers () {
+    return (
+        <>
+            <NavbarAll/>
+            <h1 className='suppliers'>SUPPLIERS</h1>
+        </>
+    )
+}
+
+export default Suppliers;
