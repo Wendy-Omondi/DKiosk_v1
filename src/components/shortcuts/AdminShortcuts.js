4e2691a5c4477f53e4dbce70523db9977559178c
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-06-08 23:04:56 +0000

    changed shortcuts to cards to match dashboard

--- src/components/shortcuts/AdminShortcuts.js
+++ src/components/shortcuts/AdminShortcuts.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import Card from "@material-ui/core/Card";
 import * as GiIcons from 'react-icons/gi';
 import * as RiIcons from 'react-icons/ri';
 import * as BsIcons from 'react-icons/bs';
 import { IconContext } from 'react-icons';
 import '../shortcuts/styles/AdminShortcuts.css'
+
 
 function AdminShortcuts() {
 
@@ -19,62 +21,62 @@ function AdminShortcuts() {
             <IconContext.Provider value={{ color: '#09143c', size: 48 }}>
                 <div className="admin-container">
                 <ul className={click ? 'admin-shortcut active' : 'admin-shortcut'}>
-                    <li className='admin-item'>
+                    <Card className='admin-item'>
                         <Link to='/product' className='admin-links'>
                             <FaIcons.FaCartPlus />
                             <br></br>
                               Inventory
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/accounts' className='admin-links'>
                             <GiIcons.GiCash />
                             <br></br>
                            Accounts
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/planning' className='admin-links'>
                             <AiIcons.AiOutlineFundProjectionScreen />
                             <br></br>
                             Projects
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/customers' className='admin-links'>
                             <RiIcons.RiCustomerService2Fill />
                             <br></br>
                             Customers
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/staff' className='admin-links'>
                             <BsIcons.BsFillPeopleFill />
                             <br></br>
                             Staff
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/marketing' className='admin-links'>
                             <FaIcons.FaShareAlt />
                             <br></br>
                             Marketing
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/accounts' className='admin-links'>
                             <GiIcons.GiCoins />
                             <br></br>
                             Sales
                         </Link>
-                    </li>
-                    <li className='admin-item'>
+                    </Card>
+                    <Card className='admin-item'>
                         <Link to='/suppliers' className='admin-links'>
                             <FaIcons.FaTruckMoving />
                             <br></br>
                             Suppliers
                         </Link>
-                    </li>
+                    </Card>
                 </ul>
                 </div>
             </IconContext.Provider>
